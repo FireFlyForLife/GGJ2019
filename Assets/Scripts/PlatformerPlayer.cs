ff385607b23f3161b1649664062515ea5dbe172a
FireFlyForLife <maiko.s.99@gmail.com>
2019-01-25 22:56:49 +0100

    Added player movement

--- Assets/Scripts/PlatformerPlayer.cs
+++ Assets/Scripts/PlatformerPlayer.cs
@@ -3,14 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformerPlayer : MonoBehaviour {
+    public float JumpSpeed = 10f;
+    public float MovementSpeed = 5f;
+    public Vector2 MaxSpeed = new Vector2(10f, 10f);
+    public float DownwardsForcePerSecond = 2.5f;
+
+    private Rigidbody playerRigidbody;
+
+    private bool canJump = true;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        playerRigidbody = GetComponent<Rigidbody>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        float hor = Input.GetAxis("Horizontal");
+        float jump = Input.GetAxisRaw("Jump");
+
+
+        {
+            var velocity = playerRigidbody.velocity;
+            velocity.x = hor * MovementSpeed;
+            playerRigidbody.velocity = velocity;
+
+            //playerRigidbody.AddForce(new Vector3(hor, 0f, 0f) * MovementSpeed);
+        }
+        if(jump == 1f && canJump)
+        {
+            playerRigidbody.AddForce(new Vector3(0f, JumpSpeed, 0f) * JumpSpeed, ForceMode.Impulse);
+            canJump = false;
+        }
+
+        playerRigidbody.AddForce(new Vector3(0f, -DownwardsForcePerSecond, 0f));
+
+        //Limit speed
+        var vel = playerRigidbody.velocity;
+        vel.x = Mathf.Clamp(vel.x, -MaxSpeed.x, MaxSpeed.x);
+        vel.y = Mathf.Clamp(vel.y, -9999999f, MaxSpeed.y);
+        playerRigidbody.velocity = vel;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //TODO: improve
+        canJump = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+    }
 }
---
+++ Assets/Scripts/SmoothCameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCameraFollow : MonoBehaviour {
+    public Transform TargetTransform;
+    public float Speed = 5f;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - TargetTransform.position;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LateUpdate()
+    {
+        //Mathf.SmoothStep
+
+        var pos = transform.position;
+        transform.position = Vector3.MoveTowards(pos, TargetTransform.position + offset, Speed);
+    }
+}
