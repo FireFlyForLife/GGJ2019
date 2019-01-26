d4d7c3d46bb494db69753a5210d825043166616b
FireFlyForLife <maiko.s.99@gmail.com>
2019-01-26 15:21:36 +0100

    Added the rotator for the player

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -10,6 +10,7 @@ public class Rotator : MonoBehaviour
     [SerializeField] private float m_rotation1;
     [SerializeField] private float m_rotation2;
     [SerializeField] private float m_rotSpeed = 5;
+    [SerializeField] private PlayerManager player;
     private float m_target;
     private bool m_isBusy = false;
 
@@ -18,23 +19,30 @@ public class Rotator : MonoBehaviour
         m_target = m_rotation1;
     }
 
+    IEnumerator TurnTowardsTarget(float rotation)
+    {
+        yield return null;
+    }
+
     void OnTriggerEnter(Collider c)
     {
         if (!m_isBusy)
         {
-            m_cam.transform.localEulerAngles =  new Vector3(0, m_target, 0);
-            c.transform.position =  new Vector3(transform.position.x, c.transform.position.y, transform.position.z);
-            m_cam.transform.position = c.transform.position + c.transform.InverseTransformVector(new Vector3(0, 0, 10));
-            if (Math.Abs(m_target - m_rotation1) < 0.001)
-            {
-                m_target = m_rotation2;
-                if(m_wallToEnable)m_wallToEnable.SetActive(false);
-            }
-            else
-            {
-                m_target = m_rotation1;
-                if (m_wallToEnable) m_wallToEnable.SetActive(true);
-            }
+            player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX;
+            m_cam.transform.parent.localEulerAngles =  new Vector3(0, m_target, 0);
+            c.transform.position = transform.position;
+            //c.transform.position =  new Vector3(transform.position.x, c.transform.position.y, transform.position.z);
+            //m_cam.transform.position = c.transform.position + c.transform.InverseTransformVector(new Vector3(0, 0, 10));
+            //if (Math.Abs(m_target - m_rotation1) < 0.001)
+            //{
+            //    m_target = m_rotation2;
+            //    if(m_wallToEnable)m_wallToEnable.SetActive(false);
+            //}
+            //else
+            //{
+            //    m_target = m_rotation1;
+            //    if (m_wallToEnable) m_wallToEnable.SetActive(true);
+            //}
         }
     }
 }
